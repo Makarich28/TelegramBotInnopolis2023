bc644e913bc595c6b6d1c5f6b8fb6dd453b355f3
Makar28 <abramovmakar72@gmail.com>
2023-06-09 10:54:16 +0300

    First commit

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ from env import TOKEN
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 
-
 logging.basicConfig(level=logging.INFO)
 
+storage = MemoryStorage()
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot, MemoryStorage())
+dp = Dispatcher(bot, storage=storage)
 
 
 @dp.message_handler(commands=["start", "help"], state='*')
@@ -30,12 +30,21 @@ async def process_name(message: Message, state: FSMContext):
 @dp.message_handler(state="q2")
 async def process_age(message: Message, state: FSMContext):
     age = message.text
-    if age.isdigit() and age >= 18:
-        await state.update_data({"age": age})
+    if age.isdigit() and int(age) >= 18:
+        age = int(age)
+        await state.update_data({"age": int(age)})
         await state.set_state("echo")
-        await bot.send_message(chat_id=message.chat.id, text=f'Hello {name}, {age}')
-        await message.answer("Now I'm echo-bot")
-    elif age.isdigit() and age < 18:
+        data = await state.get_data()
+        if age % 10 == 1 and not age % 100 == 11:
+            await bot.send_message(chat_id=message.chat.id, text=f'Hello {data["name"]}, {data["age"]} год')
+            await message.answer("Now I'm echo-bot")
+        elif age % 10 == 2 or age % 10 == 3 or age % 10 == 4 and not age % 100 == 12 and not age % 100 == 13 and not age % 100 == 14:
+            await bot.send_message(chat_id=message.chat.id, text=f'Hello {data["name"]}, {data["age"]} года')
+            await message.answer("Now I'm echo-bot")
+        else:
+            await bot.send_message(chat_id=message.chat.id, text=f'Hello {data["name"]}, {data["age"]} лет')
+            await message.answer("Now I'm echo-bot")
+    elif age.isdigit() and int(age) < 18:
         await message.answer("you are under 18 years old")
     else:
         await message.answer("This is not number. Try again")
@@ -43,7 +52,7 @@ async def process_age(message: Message, state: FSMContext):
 
 @dp.message_handler(state='echo')
 async def dont_know(message: types.Message):
-     await message.answer(f'Я не знаю что значит "{message.text}", {message.from_user["username"]}')
+    await message.answer(f'{message.text}, {message.from_user["username"]}')
 
 
 if __name__ == '__main__':
